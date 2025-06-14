74a3d50438ddf745ae8a66efbf918a3edc9bbc79
Chukwuma <echukwuma561@gmail.com>
2025-06-14 23:45:03 +0100

    Temporarily disable ESLint errors during build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ Add this line to ignore lint errors on build
+  },
 };
 
 export default nextConfig;
