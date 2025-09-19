ae1fe98af264d3cd27ce8434728f6e1a70178ab7
Chukwuma <echukwuma561@gmail.com>
2025-09-19 09:58:22 +0100

    wip

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,13 @@
 
 import type React from "react"
 
-import { Github, Linkedin, Mail, Phone, Twitter, Instagram, Video } from "lucide-react"
+import { Github, Linkedin, Mail, Phone, Twitter, Instagram } from "lucide-react"
+
+const TikTokIcon = ({ className }: { className?: string }) => (
+  <svg className={className} viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-5.2 1.74 2.89 2.89 0 0 1 2.31-4.64 2.93 2.93 0 0 1 .88.13V9.4a6.84 6.84 0 0 0-1-.05A6.33 6.33 0 0 0 5 20.1a6.34 6.34 0 0 0 10.86-4.43v-7a8.16 8.16 0 0 0 4.77 1.52v-3.4a4.85 4.85 0 0 1-1-.1z" />
+  </svg>
+)
 
 export default function Footer() {
   return (
@@ -76,7 +82,7 @@ export default function Footer() {
               <SocialLink href="https://www.instagram.com/emmanuel23670/" icon={<Instagram className="w-4 h-4" />} />
               <SocialLink
                 href="#"
-                icon={<Video className="w-4 h-4" />}
+                icon={<TikTokIcon className="w-4 h-4" />}
                 disabled={true}
                 tooltip="TikTok - Coming Soon"
               />
@@ -111,12 +117,12 @@ function SocialLink({
         title={tooltip}
         className="
           text-gray-600 cursor-not-allowed
-          p-2 rounded-full relative
+          p-2 rounded-full relative group
           hover:bg-gray-800
         "
       >
         {icon}
-        <span className="absolute -top-8 left-1/2 transform -translate-x-1/2 bg-gray-700 text-white text-xs px-2 py-1 rounded opacity-0 hover:opacity-100 transition-opacity whitespace-nowrap">
+        <span className="absolute -top-10 left-1/2 transform -translate-x-1/2 bg-gray-800 border border-gray-700 text-white text-xs px-3 py-1 rounded-md opacity-0 group-hover:opacity-100 transition-opacity duration-200 whitespace-nowrap pointer-events-none z-10">
           {tooltip}
         </span>
       </div>
